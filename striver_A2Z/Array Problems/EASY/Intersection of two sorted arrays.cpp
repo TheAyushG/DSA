3bdf73f132482b405c854417f8b7202099d55717
Ayush Gurjar <jiaayush256@gmail.com>
2026-05-21 12:34:58 +0530

    Intersection of two sorted arrays.cpp

--- striver_A2Z/Array Problems/EASY/Intersection of two sorted arrays.cpp
+++ striver_A2Z/Array Problems/EASY/Intersection of two sorted arrays.cpp
@@ -1,3 +1,50 @@
+--------------------------------------------------------------
+Time - O(n)
+space - O(1)
+--------------------------------------------------------------
+class Solution {
+  public:
+    vector<int> intersection(vector<int>& a, vector<int>& b) {
+        // code here
+        
+        int n = a.size();
+        int m = b.size();
+        
+        int i=0;
+        int j=0;
+        
+        vector<int> ans;
+        
+        while(i < n && j < m){
+            
+            if(a[i] < b[j]){
+                i++;
+            }
+            
+            else if(a[i] > b[j]){
+                j++;
+            }
+            
+            else{
+                
+                if(ans.empty() || ans.back() != a[i]){
+                    ans.push_back(a[i]);
+                }
+                
+                i++;
+                j++;
+            }
+        }
+        
+        return ans;
+    }
+};
+
+
+
+
+
+
 -----------------------------------------------------------------
 brute force code
 Time - O(n*m)
