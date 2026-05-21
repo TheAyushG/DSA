3990d8902780d3f88b0fca63d4b30658ef14e2ba
Ayush Gurjar <jiaayush256@gmail.com>
2026-05-21 18:03:59 +0530

    Find the number that appears once, and other numbers twice.cpp

---
+++ striver_A2Z/Array Problems/EASY/Find the number that appears once, and other numbers twice.cpp
@@ -0,0 +1,48 @@
+-------------------------------------------------------
+Time - O(n)
+Space - O(1)
+-------------------------------------------------------
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+    
+    int n = nums.size();
+
+    int ans = 0;
+
+    for(int i=0; i<n; i++){
+        ans ^= nums[i];
+    }
+
+    return ans;
+    
+  }
+};
+
+
+******************************************************************************
+
+---------------------------------
+Time - O(n)
+Space - O(n)
+---------------------------------
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+    
+    int n = nums.size();
+    unordered_map<int,int> map;
+    for(int i=0; i<n; i++){
+        map[nums[i]]++;
+    }
+
+    for(auto &it : map){
+        if(it.second == 1)
+        return it.first;
+    }
+
+    return -1;
+  }
+};
