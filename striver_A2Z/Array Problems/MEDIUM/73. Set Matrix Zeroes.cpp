2b3d5ba7d411b7ce492248258734547ab4ad1e1e
Ayush Gurjar <jiaayush256@gmail.com>
2026-05-30 23:26:37 +0530

    73. Set Matrix Zeroes.cpp

--- striver_A2Z/Array Problems/MEDIUM/73. Set Matrix Zeroes.cpp
+++ striver_A2Z/Array Problems/MEDIUM/73. Set Matrix Zeroes.cpp
@@ -1,3 +1,84 @@
+1. Check if first row originally has a zero.
+2. Check if first column originally has a zero.
+3. Use first row and first column as markers.
+4. Mark rows and columns that need to become zero.
+5. Update inner cells using the markers.
+6. Finally handle the first row and first column separately.
+
+-----------------------------------------------------------------
+Optimal Solution
+Time  - O(m*n) ----> O(n) + O(m) + O(m×n) + O(m×n) + O(n) + O(m)
+Space - O(1)
+-----------------------------------------------------------------
+    
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        bool firstRowImpact = false;
+        bool firstColImpact = false;
+
+        //Check first row is impacted or not
+        for(int col=0; col<n; col++){
+            if(matrix[0][col] == 0){
+                firstRowImpact = true;
+                break;
+            }
+        }
+
+        //Check first Col is impacted or not
+        for(int row=0; row<m; row++){
+            if(matrix[row][0] == 0){
+                firstColImpact = true;
+                break;
+            }
+        }
+
+        //Mark rows and columns using first row and first column
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                if(matrix[i][j] == 0){
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+
+        //Set matrix elements to 0 based on markers
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                if(matrix[0][j] == 0 || matrix[i][0] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        //Make entire first row zero if it was impacted
+        if(firstRowImpact){
+            for(int j=0; j<n; j++){
+                matrix[0][j] = 0;
+            }
+        }
+
+        //Make entire first column zero if it was impacted
+        if(firstColImpact){
+            for(int i=0; i<m; i++){
+                matrix[i][0] = 0;
+            }
+        }
+
+    }
+};
+
+
+
+*****************************************************************************************************************************************************************************************************************************
+    
+
+
 -------------------------------------------------
 Time	- O(m × n) + O(m × n)
 Space	- O(m) + O(n)
