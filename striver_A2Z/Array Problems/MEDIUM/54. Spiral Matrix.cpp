23f0d8898a9dbe83efa5e282128e0082e3f8ad9e
Ayush Gurjar <jiaayush256@gmail.com>
2026-06-01 23:05:44 +0530

    54. Spiral Matrix.cpp

--- striver_A2Z/Array Problems/MEDIUM/54. Spiral Matrix.cpp
+++ striver_A2Z/Array Problems/MEDIUM/54. Spiral Matrix.cpp
@@ -47,3 +47,69 @@ public:
         return ans;
     }
 };
+
+
+
+
+*****************************************************************************************************************************************************************************************************************************
+
+
+
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        int dir = 0;
+
+        int left = 0;
+        int right = m-1;
+        int top = 0;
+        int bottom = n-1;
+
+
+        vector<int> ans;
+
+        while(ans.size() < (m * n)){
+
+        if(dir == 0){
+            for(int i=left; i<=right; i++){
+                ans.push_back(matrix[top][i]);
+            }
+            top++;
+        }
+
+
+        if(dir == 1){
+            for(int i=top; i<=bottom; i++){
+                ans.push_back(matrix[i][right]);
+            }
+            right--;
+        }
+
+        if(dir == 2 && top <= bottom){
+            for(int i=right; i>=left; i--){
+                ans.push_back(matrix[bottom][i]);
+            }
+            bottom--;
+        }
+
+        if(dir == 3 && left <= right){
+            for(int i=bottom; i>=top; i--){
+                ans.push_back(matrix[i][left]);
+            }
+            left++;
+        }
+
+        dir++;
+
+        if(dir == 4){
+            dir = 0;
+        }
+    }
+
+        return ans;
+    }
+};
