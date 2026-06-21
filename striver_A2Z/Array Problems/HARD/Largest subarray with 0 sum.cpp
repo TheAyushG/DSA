354f1f41b97ff6aebd1d965faa87a8ba74ec8d11
Ayush Gurjar <jiaayush256@gmail.com>
2026-06-21 15:37:48 +0530

    Largest subarray with 0 sum.cpp

--- striver_A2Z/Array Problems/HARD/Largest subarray with 0 sum.cpp
+++ striver_A2Z/Array Problems/HARD/Largest subarray with 0 sum.cpp
@@ -1,3 +1,46 @@
+-------------------------------------
+Time	- O(n)
+Space	- O(n)
+-------------------------------------
+
+class Solution {
+  public:
+    int maxLength(vector<int>& arr) {
+        // code here
+        
+        int n = arr.size();
+        unordered_map<int,int> map;
+        
+        int sum = 0;
+        int maxLength = 0;
+        
+        for(int i=0; i<n; i++){
+            
+            sum += arr[i];
+            
+            if(sum == 0){
+                maxLength = i+1;
+            }
+            
+            else if(map.find(sum) != map.end()){
+                int length = i - map[sum];
+                
+                maxLength = max(maxLength, length);
+            }
+            
+            else{
+                map[sum] = i;
+            }
+        }
+        
+        return maxLength;
+    }
+};
+
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
 class Solution {
   public:
     int maxLength(vector<int>& arr) {
