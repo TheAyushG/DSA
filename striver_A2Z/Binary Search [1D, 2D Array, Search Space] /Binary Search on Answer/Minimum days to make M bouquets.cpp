bc7e8f8649f31dc8049f837f94c87c279377c560
Ayush Gurjar <jiaayush256@gmail.com>
2026-07-25 22:55:39 +0530

    Minimum days to make M bouquets.cpp

---
+++ striver_A2Z/Binary Search [1D, 2D Array, Search Space] /Binary Search on Answer/Minimum days to make M bouquets.cpp
@@ -0,0 +1,54 @@
+class Solution {
+  public:
+  
+   bool possible(vector<int>& arr, int day, int m, int k){
+
+        int count = 0;
+        int bouquets = 0;
+
+        for(int i = 0; i < arr.size(); i++){
+
+            if(arr[i] <= day){
+                count++;
+            }
+            else{
+                count = 0;
+            }
+
+            if(count == k){
+                bouquets++;
+                count = 0;      // Flowers cannot be reused
+            }
+        }
+
+        return bouquets >= m;
+    }
+    
+    
+  
+    int minDaysBloom(vector<int>& arr, int k, int m) {
+        // Code here
+        int n = arr.size();
+
+        if((long long)m * k > n){
+            return -1;
+        }
+
+        int low = *min_element(arr.begin(), arr.end());
+        int high = *max_element(arr.begin(), arr.end());
+
+        while(low <= high){
+
+            int mid = low + (high - low) / 2;
+
+            if(possible(arr, mid, m, k)){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+};
