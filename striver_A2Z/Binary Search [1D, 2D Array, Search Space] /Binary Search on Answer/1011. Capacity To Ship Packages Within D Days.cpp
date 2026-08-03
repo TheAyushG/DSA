2883c10d08fad80f764befc1ef1621b1b976342f
Ayush Gurjar <jiaayush256@gmail.com>
2026-08-03 19:44:48 +0530

    Implement helper functions for package shipping
    Added functions to calculate days, sum, and maximum weight for shipping packages.

--- striver_A2Z/Binary Search [1D, 2D Array, Search Space] /Binary Search on Answer/1011. Capacity To Ship Packages Within D Days.cpp
+++ striver_A2Z/Binary Search [1D, 2D Array, Search Space] /Binary Search on Answer/1011. Capacity To Ship Packages Within D Days.cpp
@@ -40,3 +40,79 @@ public:
         return low;
     }
 };
+
+
+
+
+
+************************************************************************************************************************************************************************************************************
+
+************* SAME CODE AS ABOVE JUST WRITE THE LOW AND HIGH FUNCTION CODE ***********************
+
+class Solution {
+public:
+    int findDays(vector<int> &weights, int capacity){
+        int days = 1;
+        int load = 0;
+
+        for(int i=0; i<weights.size(); i++){
+            if(weights[i] + load > capacity){
+                days += 1;
+                load = weights[i];
+            }
+
+            else{
+                load += weights[i];
+            }
+        }
+
+        return days;
+    }
+
+
+    int findSum(vector<int> &weights){
+        int sum = 0;
+
+        for(int i=0; i<weights.size(); i++){
+            sum += weights[i];
+        }
+
+        return sum;
+    }
+
+    int max_element(vector<int> &weights){
+        
+        int currentmax = weights[0];
+        for(int i=1; i<weights.size(); i++){
+            if(currentmax < weights[i]){
+                currentmax = weights[i];
+            }
+            
+        }
+        return currentmax;
+    }
+
+
+    int shipWithinDays(vector<int>& weights, int days) {    
+        int low = max_element(weights);
+        int high = findSum(weights);
+
+
+        while(low <= high){
+            int mid = (low + high) / 2;
+            int numberOfDays = findDays(weights, mid);
+
+            if(numberOfDays <= days){
+                high = mid - 1;
+            }
+
+            else{
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+};
+
+
