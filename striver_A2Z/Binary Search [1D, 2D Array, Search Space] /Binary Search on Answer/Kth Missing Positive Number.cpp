f430c8cc08a96399c4e13c976c3c8b484bd0405e
Ayush Gurjar <jiaayush256@gmail.com>
2026-08-06 23:21:13 +0530

    Kth Missing Positive Number.cpp

--- striver_A2Z/Binary Search [1D, 2D Array, Search Space] /Binary Search on Answer/Kth Missing Positive Number.cpp
+++ striver_A2Z/Binary Search [1D, 2D Array, Search Space] /Binary Search on Answer/Kth Missing Positive Number.cpp
@@ -1,27 +1,93 @@
 class Solution {
-  public:
-    int kthMissing(vector<int> &arr, int k) {
-        // code here
-         int current = 1;
-        int i = 0;
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        
+    int n = arr.size();
+    int mid = -1;
+    int l = 0;
+    int r = n-1;
 
-        while(k > 0){
+    while(l <= r){
+        int mid = l + (r-l)/2;
+        
+        int kitne_missing_till_mid = arr[mid] - (mid+1);
 
-            if(i < arr.size() && arr[i] == current){
-                i++;
-            }
+        if(kitne_missing_till_mid < k){
+            l = mid + 1;
+        } 
 
-            else{
-                k--;
-
-                if(k == 0){
-                    return current;
-                }
-            }
-
-            current++;
+        else{
+            r = mid - 1;
         }
+    }
 
-        return current;
+    return l+k;
+
     }
 };
+
+
+
+
+// class Solution {
+// public:
+//     int findKthPositive(vector<int>& arr, int k) {
+        
+//     int n = arr.size();
+//     int num = 1;
+//     int i = 0;
+
+//         while(i < n  && k > 0){
+
+//             if(arr[i] == num){
+//                 i++;
+//             }
+
+//             else{
+//                 k--;
+//             }
+
+//             num++;
+//         }
+
+//         while(k--){
+//             num++;
+//         }
+
+//         return num-1;
+
+//     }
+// };
+
+
+
+
+
+// class Solution {
+// public:
+//     int findKthPositive(vector<int>& arr, int k) {
+        
+//     int current = 1;
+//         int i = 0;
+
+//         while(k > 0){
+
+//             if(i < arr.size() && arr[i] == current){
+//                 i++;
+//             }
+
+//             else{
+//                 k--;
+
+//                 if(k == 0){
+//                     return current;
+//                 }
+//             }
+
+//             current++;
+//         }
+
+//         return current;
+
+//     }
+// };
